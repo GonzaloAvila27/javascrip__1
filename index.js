d21a85108d741bf9f211c9632ff435be7618a1e2
GonzaAv <gonzaloavila2727@gmail.com>
2022-04-25 09:55:02 -0300

    1

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+let aVer  = prompt ("Cuál era el nombre de pila del Gengis khan?");
+
+    while (aVer != "Temuyín"){ 
+        alert (aVer + "??? vos decis? lo que te haría Kublai...");
+        aVer = prompt ("vamos de nuevo peasant");
+    }
+    alert ("CORRECTO!! POR LA HORDA!! beltalowda💪");
+
+        
+        
